89b95bf1cf0c97015d2ac603a57c89c489218087
Ponyai30 <avixpassy@gmail.com>
2025-06-01 23:15:52 +0300

    initila commit

---
+++ JS-Fundamentals/0-javascript_is_amazing.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Script that prints "JavaScript is amazing"
+
+// Create a constant variable
+const myVar = "JavaScript is amazing";
+
+// Print the output using console.log()
+console.log(myVar);
